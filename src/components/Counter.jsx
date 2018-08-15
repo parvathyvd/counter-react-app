ee65cbe4f12334ca555a82cabc5221fd26e94224
parvathyvd <parvathyvd@gmail.com>
2018-08-15 10:18:15 -0700

    git init

---
+++ src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+    state = {  }
+    render() { 
+        return (<div>
+            <h1>Counter APP</h1>
+            <button className="btn btn-success btn-small">Increment</button>
+        </div>);
+    }
+}
+ 
+export default Counter;
